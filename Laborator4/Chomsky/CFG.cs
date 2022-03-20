4e8a7c2f619506fcc521a628ceed3718d3eb8e23
Victor <victorflor1995@gmail.com>
2022-03-20 19:03:16 +0200

    Remove null transitions

--- Laborator4/Chomsky/CFG.cs
+++ Laborator4/Chomsky/CFG.cs
@@ -9,6 +9,7 @@ namespace Chomsky
     internal class CFG
     {
         private Dictionary<string, List<string>> Transitions;
+        private HelperClass Helper = new();
         public CFG(Dictionary<string, List<string>> transitions)
         {
             Transitions = transitions;
@@ -18,6 +19,7 @@ namespace Chomsky
         {
             PrintTransitions();
             RemoveNullTransitions();
+            PrintTransitions();
         }
 
         private void PrintTransitions()
@@ -33,11 +35,70 @@ namespace Chomsky
 
                 Console.WriteLine();
             }
+
+            Console.WriteLine("--------------");
         }
 
         private void RemoveNullTransitions()
         {
+            //repeat the process of removing epsilons while any appear
+            while (Helper.HasEpsilon(Transitions))
+            {
+                var epsilonsStates = new List<string>();
+                //add Non Terminal states that have epsilon
+                foreach (var (key, list) in Transitions)
+                {
+                    if (list.Any(state => state.Equals("ε"))) 
+                        epsilonsStates.Add(key); 
+                }
 
+                //add new states that had an epsilonState
+                foreach (var epsilon in epsilonsStates)
+                {
+                    //remove the epsilon from epsilonState
+                    Transitions[epsilon].Remove("ε");
+
+                    //epsilonState = A;
+
+                    //add new states that have epsilon
+                    foreach (var (key, list) in Transitions)
+                    {
+                        int size = list.Count;
+                        for (int i = 0; i < size; i++)
+                        {
+                            //S -> A, A -> ε
+                            //in case the state maps to the epsilonState
+                            if (list[i].Equals(epsilon))
+                            {
+                                //make it an epsilonState
+                                Transitions[key][i] = "ε";
+                            }
+                            else if (list[i].Contains(epsilon))
+                            {
+                                //S -> aA, A -> ε
+                                //have to check in case we have multiple epsilonStates in the currentString
+                                var compositeEpsilon = list[i].Count(ch => ch == char.Parse(epsilon));
+                                if (compositeEpsilon == 1)
+                                {
+                                    string add = list[i].Replace(epsilon, "");
+                                    Transitions[key].Add(add);
+                                }
+                                else
+                                {
+                                    //S -> ABsA, A -> ε
+                                    //break down a big state into smaller
+                                    var smallerStates = Helper.GenerateStates(list[i], epsilon);
+                                    foreach (var state in smallerStates)
+                                    {
+                                       Transitions[key].Add(state); 
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
---
+++ Laborator4/Chomsky/HelperClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chomsky
+{
+    internal class HelperClass
+    {
+        internal bool HasEpsilon(Dictionary<string, List<string>> transitions)
+        {
+            //checks if grammar has an epsilon
+            foreach (var (_, list) in transitions)
+            {
+                if (list.Any(state => state.Equals("ε"))) return true;
+            }
+
+            return false;
+        }
+
+        internal List<string> GenerateStates(string input, string epsilonState)
+        {
+            //decompose the state into smaller states, by removing one A at a time (substring)
+            //ABAAs
+            //ABAs, BAAs
+            //ABs, BAs
+            //Bs
+            var list = new List<string>();
+            var queue = new Queue<string>();
+            queue.Enqueue(input);
+            while (queue.Count != 0)
+            {
+                string state = queue.Peek();
+                for (int i = 0; i < state.Length; i++)
+                {
+                    if (state[i] == char.Parse(epsilonState))
+                    {
+                        //take substring before that A and after that A
+                        string subState = state[..i] + state[++i..];
+                        if (!list.Contains(subState))
+                        {
+                            //handles duplicates
+                            queue.Enqueue(subState); 
+                            list.Add(subState);
+                        }
+                    }
+                }
+
+                queue.Dequeue();
+            }
+
+            return list;
+        }
+    }
+}
