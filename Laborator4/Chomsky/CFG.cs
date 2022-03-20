89d753c5c11275f2b91d90cda518b429211472a0
Victor <victorflor1995@gmail.com>
2022-03-20 15:37:33 +0200

    Started working on lab4. Created CFG class and a print method

---
+++ Laborator4/Chomsky/CFG.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chomsky
+{
+    internal class CFG
+    {
+        private Dictionary<string, List<string>> Transitions;
+        public CFG(Dictionary<string, List<string>> transitions)
+        {
+            Transitions = transitions;
+        }
+
+        public void ConvertCfGtoCnf()
+        {
+            PrintTransitions();
+            RemoveNullTransitions();
+        }
+
+        private void PrintTransitions()
+        {
+            foreach (var (key, list) in Transitions)
+            {
+                Console.Write($"{key} -> ");
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if(i < list.Count - 1) Console.Write($"{list[i]} | ");
+                    else Console.Write(list[i]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private void RemoveNullTransitions()
+        {
+
+        }
+    }
+}
---
+++ Laborator4/Chomsky/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+
+namespace Chomsky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            var lines = System.IO.File.ReadAllLines(@"D:\UTM\Anul 2\Semestrul 4\LFPC\Laborator4\Chomsky\source.txt");
+            var transitions = Initialize(lines);
+            var cfg = new CFG(transitions);
+            cfg.ConvertCfGtoCnf();
+        }
+
+        static Dictionary<string, List<string>> Initialize(string[] path)
+        { 
+            //adds grammar from file to dictionary of arrays
+            var transitions = new Dictionary<string, List<string>>();
+            foreach (var line in path)
+            {
+                if (!transitions.ContainsKey(line[0].ToString()))
+                {
+                    transitions.Add(line[0].ToString(), new List<string>());
+                    AddTransition(line);
+                }
+                else
+                {
+                    AddTransition(line);
+                }
+            }
+
+            void AddTransition(string line)
+            {
+                string substr = line.Substring(line.IndexOf('>') + 1, line.Length - 2);
+                transitions[line[0].ToString()].Add(substr);
+            }
+
+            return transitions;
+        }
+
+    }
+}
